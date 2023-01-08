a30beca3ec2663105ba9e8d989b430fd75ea8cbd
kirkor313 <kirkor313@mail.ru>
2023-01-08 23:38:06 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
-  
+  let firstTwoTens = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+  let tens = ["","","twenty","thirty","forty","fifty","sixty", "seventy","eighty","ninety"];
+  let hndr = "hundred"
+  if (number >=0 && number < 20) {
+     return firstTwoTens[number];
+  }
+  if ((number % 10 == 0) && number < 100) {
+    return tens[number/10];
+  }
+  if ((number % 100 == 0) && (number < 1000 && number >= 100)) {
+      return `${firstTwoTens[number/100]} ${hndr}`;
+  }
+  if (number < 100 && number > 20) { 
+    return `${tens[Math.trunc(number/10)]} ${firstTwoTens[number % 10]}`;
+  }
+  if ((number > 100 && number < 1000) && (number % 10 !== 0 && number % 100 > 20)) {
+    return `${firstTwoTens[Math.trunc(number/100)]} ${hndr} ${tens[Math.trunc((number % 100)/10)]} ${firstTwoTens[number % 10]}`;
+  }
+  if ((number > 100 && number < 1000) && (number % 10 == 0 && number % 100 !== 10)) {
+    return `${firstTwoTens[Math.trunc(number/100)]} ${hndr} ${tens[Math.trunc((number % 100)/10)]}`;
+  }
+  if ((number > 100 && number < 1000) && (number % 10 !== 0 && number % 100 < 10)) {
+    return `${firstTwoTens[Math.trunc(number/100)]} ${hndr} ${firstTwoTens[number % 10]}`;
+  }
+  if ((number > 100 && number < 1000) && (number % 100 > 10 && number % 100 < 20)) {
+    return `${firstTwoTens[Math.trunc(number/100)]} ${hndr} ${firstTwoTens[(number % 10) + 10]}`;
+  }
+  if ((number > 100 && number < 1000) && (number % 100 == 10)) {
+    return `${firstTwoTens[Math.trunc(number/100)]} ${hndr} ${firstTwoTens[10]}`;
+  }
 }
